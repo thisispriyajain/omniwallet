b992abc7d8b0d22cb213d4229e6eb5ea6afd6de9
Priya Jain <37912424+thisispriyajain@users.noreply.github.com>
2024-12-03 19:22:52 -0800

    Priya's commits (+1)

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:omniwallet/blocs/authentication/bloc/authentication_bloc.dart';
 import 'package:omniwallet/firebase_options.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:omniwallet/navigation/routerdemo.dart';
 import 'package:omniwallet/pages/router_pages/home_page.dart';
 import 'my_home_page.dart';
